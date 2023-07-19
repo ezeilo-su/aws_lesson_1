75dcc16b2ad49daf9e23d1bef08ab507fa77ef57
Sunday Ezeilo <ezeilo.su@gmail.com>
2023-07-19 10:45:51 +0100

    Use default region from cli

--- bin/lesson1.ts
+++ bin/lesson1.ts
@@ -15,7 +15,7 @@ new Lesson1Stack(app, 'Lesson1Stack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '820187478789', region: 'eu-west-1' }
+  // env: { account: '820187478789', region: 'eu-west-1' }
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 
